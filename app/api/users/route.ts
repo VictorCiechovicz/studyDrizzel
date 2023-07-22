25bb8168fa6349e7d8f8141d01282326acad7c14
VictorCiechovicz <victor09avila@hotmail.com>
2023-07-22 10:05:36 -0300

    feat: create router handler delete

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import db from "@/db";
 import { User, NewUser, users } from '../../../schemas/user'
+import { eq } from "drizzle-orm";
 
 
 export async function GET(request: NextRequest) {
@@ -23,9 +24,33 @@ export async function POST(request: NextRequest) {
 }
 
 export async function PUT(request: NextRequest) {
-  return NextResponse.json({});
+  const userToUpdate: User = await request.json()
+  const user = await db.select().from(users).where(eq(users.id, userToUpdate.id)) //busca e relaciona o usuario
+
+  const updatedUser = await db.update(users).set({
+    ...user[0],
+    ...userToUpdate //o select acima retorna um array,neste caso estamos pegadno o indice zero do array e substituindo ele
+  }).where(eq(users.id, userToUpdate.id)).returning(
+    {
+      id: users.id,
+      name: users.name,
+      email: users.email,
+      createdAt: users.createdAt
+    }
+  )
+
+  return NextResponse.json({ users: updatedUser });
 }
 
 export async function DELETE(request: NextRequest) {
-  return NextResponse.json({});
+  const userToDelete: User = await request.json()
+  const deletedUser = await db.delete(users).where(eq(users.id, userToDelete.id)).returning(
+    {
+      id: users.id,
+      name: users.name,
+      email: users.email,
+      createdAt: users.createdAt
+    }
+  )
+  return NextResponse.json({ users: deletedUser });
 }
